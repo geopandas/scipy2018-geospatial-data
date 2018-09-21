c2d624615d3f9672938461ac47c6fbd0ab950332
Pranjal Dhole <dhole.pranjal@gmail.com>
2018-09-21 16:02:21 +0200

    import errors related to libpysal and mapclassify fixed [issue #19]

--- _solved/solutions/case-trump-vote09.py
+++ _solved/solutions/case-trump-vote09.py
@@ -1,4 +1,4 @@
 f = plt.figure(figsize=(6,6))
-plt.scatter(pres.swing_full, lp.lag_spatial(w, pres.swing_full))
+plt.scatter(pres.swing_full, lp.weights.lag_spatial(w, pres.swing_full))
 plt.plot((-.3,.1),(-.3,.1), color='k')
 plt.title('$I = {:.3f} \ \ (p < {:.3f})$'.format(moran.I,moran.p_sim))
